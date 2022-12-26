672352ac204d17638abdda9d089c6d532b4d939f
xmiyabix <xxx@users.noreply.github.com>
2022-12-26 23:55:30 +0900

    テーブルのサンプルをつけた

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Tabletest from './Tabletest'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Tabletest />
   </React.StrictMode>,
 )
