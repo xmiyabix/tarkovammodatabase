42299f9954d5d4eb0c043d1f7b0899a7caa419e2
xmiyabix <xxx@users.noreply.github.com>
2022-12-26 23:42:36 +0900

    細かい修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Button } from "@mui/material";
 
+
+const  App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <>
+      <Button>text</Button>
+      <Button variant="contained">contained</Button>
+      <Button variant="outlined">outlined</Button>
+    </>
+  );
+};
+
+
+
 
 export default App
