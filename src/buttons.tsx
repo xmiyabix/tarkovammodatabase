73941b8b8acce4a12f111c851657f48f1e0a2eec
xmiyabix <78207562+xmiyabix@users.noreply.github.com>
2023-01-04 15:34:21 +0900

    弾丸のボタンを全て実装した

--- src/buttons.tsx
+++ src/buttons.tsx
@@ -7,6 +7,8 @@ const Buttons = () => {
     const [ammodata, setAmmodata] = useState<TableEntry[]>([]);//TableEntry型の空配列を渡してあげている
     return (
       <>
+        Pistol Ammos
+        <br/>
         <Button variant="outlined"
             onClick={() => {
             const tmpname :string[] = ['9*19']
@@ -34,7 +36,134 @@ const Buttons = () => {
             setAmmodata(getAmmodata(tmpname));
             }}>
             45ACP</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['9*21']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            9×21mm</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['4.6*30']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            4.6×30mm</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['5.7*28']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            5.7×28mm</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['357magnum']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            357Magnum</Button>
+            <br/>
+            RifleAmmos
+            <br/>
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['5.45*39']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            5.45×39mm</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['5.56*45']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            5.56×45mm</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['7.62*39']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            7.62×39mm</Button>
+            
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['7.62*51']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            7.62×51mm</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['366tkm']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            366TKM</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['7.62*51']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            7.62×51mm</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['9*39']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            9×39mm</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['300blackout']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            300Blackout</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['12.7*55']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            12.7×55mm</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['338lapua']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            338lapua</Button>
+
+            <br/>
+            ShotgunAmmos
+            <br/>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['12gauge']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            12gauge</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['20gauge']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            20gauge</Button>
+
+            <Button variant="outlined"
+            onClick={() => {
+            const tmpname :string[] = ['8gauge']
+            setAmmodata(getAmmodata(tmpname));
+            }}>
+            8gauge</Button>
+
             <Tabletest entries={ammodata} />
+            
       </>
     );
   };
